1e9979341346b29b6b237929db0a05adb3613690
Nathan Francis <nathandfran06@gmail.com>
2020-10-03 15:59:56 -0400

    Added a background
    Added the grid backgroud and a snake class and a green dot representing the snake.

--- snakeGM.py
+++ snakeGM.py
@@ -1,10 +1,10 @@
-from pygame import *
+import pygame
 
 pygame.init()
 
 #ScreenXandY
-Screenwid= 500
-Screenht=500
+Screenwid= 710
+Screenh=500
 
 #Screen is the win
 win = pygame.display.set_mode((Screenwid, Screenh))
@@ -15,9 +15,25 @@ pygame.display.set_caption('Snake')
 #Frame rate
 clock= pygame.time.Clock()
 
+#Background
+bg=pygame.image.load('C:\\Users\\18597\\Desktop\\My Python Scripts\\snake\\board.png')
+
+#snake class
+class snake(object):
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        self.score=0
+
+    def draw(self,win):
+        pygame.draw.rect(win,(0,128,0),(self.x, self.y, 34,34))
+
+
 def drawWin():
     win.blit(bg,(0,0))
-
+    slither.draw(win)
+    pygame.display.update()
+    clock.tick(15)
 def gameLoop():
     run=True
     while run:
@@ -25,6 +41,8 @@ def gameLoop():
             if event.type==pygame.QUIT:
                 pygame.quit()
                 quit()
+        global slither
+        slither=snake(318,253)
         drawWin()
 
 

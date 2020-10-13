32411e3efb694a8525005d6c76481dfefecaeb69
Nathan Francis <nathandfran06@gmail.com>
2020-10-12 20:03:29 -0400

    Updated Movement working and Created food for snake
    Working on COllision for the food and snake

--- snakeGM.py
+++ snakeGM.py
@@ -2,11 +2,17 @@ import pygame
 
 import time
 
+import random
+
 pygame.init()
 
 #ScreenXandY
-Screenwid= 710
-Screenh=500
+Screenwid= 300
+Screenh=300
+
+#List of possible locatons for food
+appleX=[10,50,100,150,200,250]
+appleY=[10,50,100,150,200,250]
 
 #Screen is the win
 win = pygame.display.set_mode((Screenwid, Screenh))
@@ -31,11 +37,25 @@ class snake(object):
         self.y_change=0
 
 
-#Background Color
-win.fill((255,255,255))
+class food(object):
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        self.width=10
+        self.height=10
+    def score(self):
+        if slither.x== self.x-10 <= self.x <= self.x+10 and slither.y ==  self.y-10<=self.y<=self.y+10:
+            slither.score= +1
+            self.x= random.choice(appleX)
+            self.y= random.choice(appleY)
+            print(slither.score)
+    def draw(self,win):
+        pygame.draw.rect(win,(0,0,0),[self.x,self.y,self.width,self.height])
  
 global slither
-slither=snake(318,253)
+slither=snake(Screenwid/2,Screenh/2)
+
+apple=food(270,260)
 
 
 def gameLoop():
@@ -51,23 +71,26 @@ def gameLoop():
                     slither.x_change= -10
                     slither.y_change= 0
             
-                if pygame.key==pygame.K_RIGHT:
+                if event.key==pygame.K_RIGHT:
                     slither.x_change= 10
                     slither.y_change= 0
 
-                if pygame.key==pygame.K_UP:
+                if event.key==pygame.K_UP:
                     slither.x_change= 0
                     slither.y_change= -10
 
-                if pygame.key==pygame.K_DOWN:
+                if event.key==pygame.K_DOWN:
                     slither.x_change= 0
                     slither.y_change= 10
 
         slither.x += slither.x_change
         slither.y += slither.y_change
+        win.fill((255,255,255))
         pygame.draw.rect(win,(0,128,0),[slither.x, slither.y, slither.width, slither.height])
+        apple.draw(win)
+        apple.score()
         pygame.display.update()
-        clock.tick(30)
+        clock.tick(15)
 
 
 

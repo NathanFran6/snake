2bbd3e9e86d5f623ff81167e44814e3e92ffabb9
Nathan Francis <nathandfran06@gmail.com>
2020-10-13 21:20:41 -0400

    Made movement and body for snake

--- snakeGM.py
+++ snakeGM.py
@@ -30,12 +30,19 @@ class snake(object):
     def __init__(self,x,y):
         self.x=x
         self.y=y
-        self.width=10
-        self.height=10
         self.score=0
         self.x_change=0
         self.y_change=0
+        self.length=1
 
+snakeBlock=10
+snakeMulti=[]
+
+def multiSnake(snakeBlock, snakeMulti):
+    for x in snakeMulti:
+        pygame.draw.rect(win, (0,128,0), [x[0], x[1], snakeBlock, snakeBlock])
+
+ 
 
 class food(object):
     def __init__(self,x,y):
@@ -44,18 +51,17 @@ class food(object):
         self.width=10
         self.height=10
     def score(self):
-        if slither.x== self.x-10 <= self.x <= self.x+10 and slither.y ==  self.y-10<=self.y<=self.y+10:
-            slither.score= +1
-            self.x= random.choice(appleX)
-            self.y= random.choice(appleY)
-            print(slither.score)
-    def draw(self,win):
-        pygame.draw.rect(win,(0,0,0),[self.x,self.y,self.width,self.height])
- 
+        if slither.x == self.x and slither.y == self.y:
+            slither.length += 1
+            self.x=round(random.randrange(0, Screenwid - snakeBlock) / 10.0) * 10.0 
+            self.y=round(random.randrange(0, Screenwid - snakeBlock) / 10.0) * 10.0
+
 global slither
 slither=snake(Screenwid/2,Screenh/2)
 
-apple=food(270,260)
+global apple
+apple=food(round(random.randrange(0, Screenwid - snakeBlock) / 10.0) * 10, 
+            round(random.randrange(0, Screenwid - snakeBlock) / 10.0) * 10.0)
 
 
 def gameLoop():
@@ -82,14 +88,25 @@ def gameLoop():
                 if event.key==pygame.K_DOWN:
                     slither.x_change= 0
                     slither.y_change= 10
+        snakeHead=[]
+        snakeHead.append(slither.x)
+        snakeHead.append(slither.y)
+        snakeMulti.append(snakeHead)
+        if len(snakeMulti)> slither.length:
+            del snakeMulti[0]
 
+        for x in snakeMulti[:-1]:
+            if x == snakeHead:
+                print('Game Over')
+        
         slither.x += slither.x_change
         slither.y += slither.y_change
         win.fill((255,255,255))
-        pygame.draw.rect(win,(0,128,0),[slither.x, slither.y, slither.width, slither.height])
-        apple.draw(win)
+        multiSnake(snakeBlock, snakeMulti)
+        pygame.draw.rect(win,(0,128,0),[slither.x, slither.y, snakeBlock, snakeBlock])
+        pygame.draw.rect(win,(0,0,0),[apple.x,apple.y,snakeBlock, snakeBlock])
         apple.score()
-        pygame.display.update()
+        pygame.display.update() 
         clock.tick(15)
 
 
